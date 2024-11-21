4ed7dced38881c1554e23e48143f9b33dad26f84
3cGiAysla <00001103061392sp@al.educacao.sp.gov.br>
2024-11-21 11:11:10 -0300

    Add files via upload

---
+++ ferramenta de estudos - Gi 2/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Língua Portuguesa',
+    'O que é figura de linguagem?',
+    'palavras ou expressões conotativas, portanto, apresentam um sentido que ultrapassa a linguagem comum, literal ou denotativa.'
+)
+
+criaCartao(
+    'Programação',
+    'o que e javscript',
+    'e uma linguagem de programação que pode adicionar interatividade no site'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amarelo em Inglês?',
+    'amarelo em ingles é yellow(ielou)'
+)
